a489785e8527e8fda08fd90d9e9a9255f0a4fef0
Anmol Gupta <gupta.28@iitj.ac.in>
2020-11-26 21:47:54 +0530

    Form

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app = express();
 const port = process.env.PORT;
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json())
+const urlencodedParser = bodyParser.urlencoded({extended:false});
 app.use(routes);
+app.post('/token', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render('token-received', {data:req.body, title: 'token-saved', message: 'Token saved successfully'});
+  });
 app.listen(port, () => console.log(`Quickstart app listening at http://localhost:${port}`))
 
 module.exports = app;
